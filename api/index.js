6fcd07edc95a4ef88323f19ee3347800c7ccfa31
Kerim Ceylan <kerimceylan01@gmail.com>
2023-04-28 16:30:26 +0300

    Deneme1

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.send('Hello from API route jababababa!');
+  res.send('Deneme Son Durum');
 });
 
 app.listen(3000, () => {
