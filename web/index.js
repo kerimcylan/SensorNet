12bd95ba7272cf9d8778a85334794f4d58d7c62e
kerimcylan <71334905+kerimcylan@users.noreply.github.com>
2023-05-24 22:24:47 +0300

    workflow deneme 45435435

--- web/index.js
+++ web/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Web server!');
+  res.send('Web Sunucusuuuuuuuuuuuuu!');
 });
 
 app.get('/api', (req, res) => {
