1d329ddcff401992379ab579190d144724f3c5ca
Kerim Ceylan <kerimceylan01@gmail.com>
2023-05-24 22:44:45 +0300

    TEkrar deneme

--- web/index.js
+++ web/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hehueuheuhfdhgdgfdfdfdeuheeuh!');
+  res.send('Son Deneme artık yeter lütfen!');
 });
 
 app.get('/api', (req, res) => {
