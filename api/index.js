cd1b301a8134c79aaba177aa633c190ad09abd13
Kerim Ceylan <kerimceylan01@gmail.com>
2023-05-30 02:00:28 +0300

    Bağlantı deneme

--- api/index.js
+++ api/index.js
@@ -1,9 +1,46 @@
-const express = require('express');
+//PACKAGES
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require('cors')
 
-app.get('/api', (req, res) => {
-  res.send('Serap görsüns');
+
+// To be able to execute server "app.js" added to scripts in package.json
+
+
+
+require('dotenv/config');
+
+//MIDDLEWARES
+app.use(cors());
+app.use(bodyParser.json());
+
+//app.use('/posts', () => {
+
+  //  console.log("This is a middelware running.")
+//});
+
+//IMPORT ROUTES
+const postsRoute = require('./routes/posts')
+
+app.use('/posts', postsRoute);
+
+
+
+//ROUTES  get:get the info  post:give the info  delete patch:updates 
+app.get('/', (req,res) => {
+    res.send("We are on home and system can connect to the data base.");
 });
+
+
+//CONNECT TO DB
+mongoose.connect(
+    process.env.DB_CONNECTION, 
+    { useNewURLparser: true},
+    () => console.log('connected to DB')
+);
+
 
 app.listen(3000, () => {
   console.log('API server is listening on port 3000');
