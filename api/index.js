9d489758f9a2fab6f0b7a18b7dc6152ee6e66f86
Kerim Ceylan <kerimceylan01@gmail.com>
2023-05-30 12:59:20 +0300

    Yeni deneme

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const app = express();
+
+require('dotenv/config');
+
 
 app.get('/api', (req, res) => {
   res.send('Serap görsüns');
 });
+
+//CONNECT TO DB
+mongoose.connect(
+  process.env.DB_CONNECTION, 
+  { useNewURLparser: true},
+  () => console.log('connected to DB')
+);
 
 app.listen(3000, () => {
   console.log('API server is listening on port 3000');
