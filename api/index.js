90866887984068a28d79f2e4f886082a1ca77061
Kerim Ceylan <kerimceylan01@gmail.com>
2023-06-01 15:07:47 +0300

    Eheheh

--- api/index.js
+++ api/index.js
@@ -5,24 +5,6 @@ const app = express();
 require('dotenv/config');
 mongoose.set('strictQuery', false);
 
-// Define the schema for your collection
-const DateSchema = new mongoose.Schema({
-  NitricOxide: Number,
-});
-
-const Date = mongoose.model('Date', DateSchema);
-
-app.get('/api', (req, res) => {
-  // Use the appropriate query to find documents with Nitric oxide less than 5
-  Date.find({ NitricOxide: { $lt: 5 } })
-    .then((results) => {
-      res.json(results);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: 'An error occurred while querying the database' });
-    });
-});
-
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to the database');
@@ -34,5 +16,47 @@ mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedT
   .catch((error) => {
     console.error('Failed to connect to the database:', error);
   });
+
+
+  const DataSchema = new mongoose.Schema({
+    _id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    Date: {
+      type: String,
+      required: true,
+    },
+    "Nitric oxide": {
+      type: Number,
+      required: true,
+    },
+    Status: {
+      type: String,
+      required: true,
+    },
+    "Nitrogen dioxide": {
+      type: Number,
+      required: true,
+    },
+    "Nitrogen oxides as nitrogen dioxide": {
+      type: Number,
+      required: true,
+    },
+  });
+
+  const DataModel = mongoose.model('Data', DataSchema);
+
+app.get('/api', (req, res) => {
+  // Use the appropriate query to find documents with Nitric oxide less than 5
+  DataModel.find({ "Nitric oxide": { $lt: 5 } })
+    .then((results) => {
+      res.json(results);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'An error occurred while querying the database' });
+    });
+});
+
 
 
