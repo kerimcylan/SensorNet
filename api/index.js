b92d1c4d89ef3226323d4c82f9eb5604ba9232b8
srptas <seraptas@yandex.com>
2023-06-01 17:45:59 +0300

    api init

--- api/index.js
+++ api/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send('Serap görsüns');
+require('dotenv/config');
+
+
+app.get('/api', async (req, res) => {
+  try {
+    const data = await mongoose.connection.db.collection('dummy').find().toArray();
+
+    res.json(data);
+  } catch (err) {
+    console.error('Query Error: ', err);
+    res.status(500).send('Query Error ');
+  }
 });
+
+//CONNECT TO DB
+mongoose.connect(
+  process.env.DB_CONNECTION, 
+  { useNewURLparser: true},
+  () => console.log('connected to Database')
+);
 
 app.listen(3000, () => {
   console.log('API server is listening on port 3000');
