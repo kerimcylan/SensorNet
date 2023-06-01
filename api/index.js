8f9c88c304cac1d5b985c41d17d3af0a97789071
Kerim Ceylan <kerimceylan01@gmail.com>
2023-06-01 17:00:28 +0300

    Uğraştım

--- api/index.js
+++ api/index.js
@@ -4,10 +4,16 @@ const mongoose = require("mongoose");
 
 const dbURI = 'mongodb+srv://khassensor:Nt!51f3!k@khassensornetwork.biwmfgq.mongodb.net/?retryWrites=true&w=majority';
 
-
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB\'ye bağlandı');
+
+    const dummySchema = new mongoose.Schema({
+      name: String,
+      age: Number
+    });
+
+    const DummyModel = mongoose.model('Dummy', dummySchema);
 
     // Örnek verileri ekle
     const dummyData = [
@@ -22,35 +28,30 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   })
   .catch((error) => console.error('MongoDB bağlantısı başarısız:', error));
 
-const dummySchema = new mongoose.Schema({
-  name: String,
-  age: Number
+app.get('/api', (req, res) => {
+  res.send('Deneme');
 });
-  
-  app.get('/api', (req, res) => {
-    res.send('Deneme');
-  });
-  
-  app.get('/api/examples', async (req, res) => {
-    try {
-      const examples = await DummyModel.find();
-      res.json(examples);
-    } catch (error) {
-      console.error('Koleksiyon sorgulanırken hata oluştu', error);
-      res.status(500).json({ error: 'İç sunucu hatası' });
-    }
-  });
-  
-  app.get('/api/query', async (req, res) => {
-    try {
-      const queryResult = await DummyModel.find({ age: { $gt: 25 } });
-      res.json(queryResult);
-    } catch (error) {
-      console.error('Koleksiyon sorgulanırken hata oluştu', error);
-      res.status(500).json({ error: 'İç sunucu hatası' });
-    }
-  });
-  
-  app.listen(3000, () => {
-    console.log('API sunucusu 3000 numaralı portta dinleniyor');
-  });
+
+app.get('/api/examples', async (req, res) => {
+  try {
+    const examples = await DummyModel.find();
+    res.json(examples);
+  } catch (error) {
+    console.error('Koleksiyon sorgulanırken hata oluştu', error);
+    res.status(500).json({ error: 'İç sunucu hatası' });
+  }
+});
+
+app.get('/api/query', async (req, res) => {
+  try {
+    const queryResult = await DummyModel.find({ age: { $gt: 25 } });
+    res.json(queryResult);
+  } catch (error) {
+    console.error('Koleksiyon sorgulanırken hata oluştu', error);
+    res.status(500).json({ error: 'İç sunucu hatası' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('API sunucusu 3000 numaralı portta dinleniyor');
+});
