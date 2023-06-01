d435891e9ee9104fb8694e6e68bbf6ee1c57c2cd
Kerim Ceylan <kerimceylan01@gmail.com>
2023-06-01 16:57:29 +0300

    ağağağağağağa

--- api/index.js
+++ api/index.js
@@ -1,48 +1,56 @@
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
-require('dotenv/config');
 
 const dbURI = 'mongodb+srv://khassensor:Nt!51f3!k@khassensornetwork.biwmfgq.mongodb.net/?retryWrites=true&w=majority';
 
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB\'ye bağlandı'))
+  .then(() => {
+    console.log('MongoDB\'ye bağlandı');
+
+    // Örnek verileri ekle
+    const dummyData = [
+      { name: 'John Doe', age: 30 },
+      { name: 'Jane Smith', age: 28 },
+      { name: 'Bob Johnson', age: 35 }
+    ];
+
+    DummyModel.insertMany(dummyData)
+      .then(() => console.log('Örnek veriler eklendi'))
+      .catch((error) => console.error('Örnek veri ekleme hatası:', error));
+  })
   .catch((error) => console.error('MongoDB bağlantısı başarısız:', error));
 
-// MongoDB koleksiyonu için şema ve model oluştur
-const exampleSchema = new mongoose.Schema({
+const dummySchema = new mongoose.Schema({
   name: String,
   age: Number
 });
-const ExampleModel = mongoose.model('Example', exampleSchema);
-
-app.get('/api', (req, res) => {
-  res.send('Deneme');
-});
-
-app.get('/api/examples', async (req, res) => {
-  try {
-    // Koleksiyondaki tüm belgeleri sorgula ve al
-    const examples = await ExampleModel.find();
-    res.json(examples);
-  } catch (error) {
-    console.error('Koleksiyon sorgulanırken hata oluştu', error);
-    res.status(500).json({ error: 'İç sunucu hatası' });
-  }
-});
-
-app.get('/api/query', async (req, res) => {
-  try {
-    // Belirli kriterlere göre koleksiyonda sorgu yap
-    const queryResult = await ExampleModel.find({ age: { $gt: 25 } });
-    res.json(queryResult);
-  } catch (error) {
-    console.error('Koleksiyon sorgulanırken hata oluştu', error);
-    res.status(500).json({ error: 'İç sunucu hatası' });
-  }
-});
-
-app.listen(3000, () => {
-  console.log('API sunucusu 3000 numaralı portta dinleniyor');
-});
+  
+  app.get('/api', (req, res) => {
+    res.send('Deneme');
+  });
+  
+  app.get('/api/examples', async (req, res) => {
+    try {
+      const examples = await DummyModel.find();
+      res.json(examples);
+    } catch (error) {
+      console.error('Koleksiyon sorgulanırken hata oluştu', error);
+      res.status(500).json({ error: 'İç sunucu hatası' });
+    }
+  });
+  
+  app.get('/api/query', async (req, res) => {
+    try {
+      const queryResult = await DummyModel.find({ age: { $gt: 25 } });
+      res.json(queryResult);
+    } catch (error) {
+      console.error('Koleksiyon sorgulanırken hata oluştu', error);
+      res.status(500).json({ error: 'İç sunucu hatası' });
+    }
+  });
+  
+  app.listen(3000, () => {
+    console.log('API sunucusu 3000 numaralı portta dinleniyor');
+  });
