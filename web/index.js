b17278dedff5a4f14b0a71ce9dfb38a70d53fa1a
Kerim Ceylan <kerimceylan01@gmail.com>
2023-06-01 15:12:48 +0300

    Sıkıldım

--- web/index.js
+++ web/index.js
@@ -5,7 +5,9 @@ app.get('/', (req, res) => {
   res.send('Serap görsün!');
 });
 
-
+app.get('/api', (req, res) => {
+  res.send('Deneme');
+});
 
 app.listen(3000, () => {
   console.log('Web server is listening on port 3000');
