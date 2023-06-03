9ae22039b09cd81cb2da6f7994af95f4b65f72c1
srptas <seraptas@yandex.com>
2023-06-03 16:45:47 +0300

    api index fix

--- api/index.js
+++ api/index.js
@@ -1,28 +1,44 @@
-const express = require('express');
-const mongoose = require("mongoose");
+// To be able to execute server "app.js" added to scripts in package.json
+
+//PACKAGES
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require('cors')
 
 require('dotenv/config');
 
+//MIDDLEWARES
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/api', async (req, res) => {
-  try {
-    const data = await mongoose.connection.db.collection('dummy').find().toArray();
+//app.use('/posts', () => {
 
-    res.json(data);
-  } catch (err) {
-    console.error('Query Error: ', err);
-    res.status(500).send('Query Error ');
-  }
+  //  console.log("This is a middelware running.")
+//});
+
+//IMPORT ROUTES
+const postsRoute = require('./routes/posts')
+
+app.use('/posts', postsRoute);
+
+
+//ROUTES  get:get the info  post:give the info  delete patch:updates 
+app.get('/', (req,res) => {
+    res.send("We are on home");
 });
+
 
 //CONNECT TO DB
 mongoose.connect(
-  process.env.DB_CONNECTION, 
-  { useNewURLparser: true},
-  () => console.log('connected to Database')
+    process.env.DB_CONNECTION, 
+    { useNewURLparser: true},
+    () => console.log('connected to DB')
 );
 
-app.listen(3000, () => {
-  console.log('API server is listening on port 3000');
-});
+
+
+//How to we start listening to the server
+app.listen(3000);
+
