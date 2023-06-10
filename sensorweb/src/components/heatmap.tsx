a192bc1b802d0297a926c6732c7f34877d7f24a2
muradmolla <muradmollaoglu@gmail.com>
2023-06-10 20:19:34 +0300

    Heatmap TS type definations

--- sensorweb/src/components/heatmap.tsx
+++ sensorweb/src/components/heatmap.tsx
@@ -3,29 +3,36 @@ import h337, { DataPoint, HeatmapData } from "heatmap.js";
 import styles from "./heatmap.module.css";
 import { scalePointsFromData } from "@/helpers/location";
 
-const HeatMap = ({ mapData }) => {
+interface heatmapProps {
+  mapData: DataPoint<"x", "y", "value">[];
+}
+
+const HeatMap = (props: heatmapProps) => {
   const containerRef = React.useRef(null);
 
   const newPoints = (el: HTMLElement) => {
     const points = [];
 
-    for (let i = 0; i < mapData.length; i++) {
+    for (let i = 0; i < props.mapData.length; i++) {
       let newPoints = scalePointsFromData(
-        mapData[i].x,
-        mapData[i].y,
+        props.mapData[i].x,
+        props.mapData[i].y,
         el.offsetWidth,
         el.offsetHeight
       );
       points.push({
         x: newPoints[0],
         y: newPoints[1],
-        value: mapData[i].value,
+        value: props.mapData[i].value,
       });
-      }
-      return points;
+    }
+    return points;
   };
 
-  const renderHeatmap = (el: HTMLElement, points) => {
+  const renderHeatmap = (
+    el: HTMLElement,
+    points: DataPoint<"x", "y", "value">[]
+  ) => {
     var config = {
       container: el,
       radius: 50,
@@ -40,21 +47,21 @@ const HeatMap = ({ mapData }) => {
       min: 0,
       data: points,
     });
-      return heatmapInstance;
+    return heatmapInstance;
   };
   useEffect(() => {
     if (containerRef.current) {
       const element: HTMLElement = containerRef.current!;
-        const heatmap = renderHeatmap(element, newPoints(element));
+      const heatmap = renderHeatmap(element, newPoints(element));
 
-        const renderHeatmapEvent = () => {
-            heatmap.setData({
-              max: 25,
-              min: 0,
-              data: newPoints(element),
-            });
-        }
-        addEventListener("resize", renderHeatmapEvent);
+      const renderHeatmapEvent = () => {
+        heatmap.setData({
+          max: 25,
+          min: 0,
+          data: newPoints(element),
+        });
+      };
+      addEventListener("resize", renderHeatmapEvent);
     }
   });
 
