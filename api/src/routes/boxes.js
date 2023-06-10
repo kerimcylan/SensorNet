8f7d29ac702919f7afcbd59757d4e47ef71b40be
muradmolla <muradmollaoglu@gmail.com>
2023-06-10 15:26:30 +0300

    Master Box Test Endpoint

--- api/src/routes/boxes.js
+++ api/src/routes/boxes.js
@@ -2,7 +2,6 @@ const { Router } = require("express");
 const express = require("express");
 const { restart } = require("nodemon");
 
-
 const router = express.Router();
 const Box = require("../models/Box");
 
@@ -24,8 +23,26 @@ router.post("/", async (req, res) => {
   //console.log(post);
 });
 
-router.get("/", async (req, res) => {
-      res.send("test");
+// Dummy data testing for masterbox
+router.get("/test", async (req, res) => {
+  const fs = require("fs");
+  const path = require("path");
+  fs.readFile(path.resolve(__dirname, '../storage/last_data.json'), (err, data) => {
+    if (err) res.send(err);
+    else {
+      let lastData = JSON.parse(data);
+      res.send(lastData);
+    }
+  });
+  return "hehe";
+});
+
+router.post("/test", async (req, res) => {
+  res.send("OK");
+  const data = JSON.stringify(req.body);
+  const fs = require("fs");
+  const path = require("path");
+  fs.writeFileSync(path.resolve(__dirname, "../storage/last_data.json"), data);
   return "hehe";
 });
 
