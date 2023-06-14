7868e4790429a5dde0d1f53fbf42b261e9bde330
muradmolla <muradmollaoglu@gmail.com>
2023-06-15 01:09:54 +0300

    cross origin warning fix

--- sensorweb/src/pages/_app.js
+++ sensorweb/src/pages/_app.js
@@ -10,7 +10,7 @@ const MyApp = ({ Component, pageProps }) => {
         <title>Khas Sensor Network</title>
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
         <link href="https://fonts.googleapis.com/css2?family=Alumni+Sans+Collegiate+One:ital@0;1&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet"/>
 
       </Head>
